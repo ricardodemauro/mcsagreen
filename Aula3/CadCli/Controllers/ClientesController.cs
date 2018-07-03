2317c032bdac49a7254af44349dddb745c1e030a
Ricardo De Mauro <ricardo.demauro@coxautoinc.com>
2018-07-03 17:49:17 -0300

    second commit

---
+++ Aula3/CadCli/Controllers/ClientesController.cs
@@ -0,0 +1,41 @@
+﻿using CadCli.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace CadCli.Controllers
+{
+    public class ClientesController : Controller
+    {
+        public ActionResult Index()
+        {
+            //Acessar base de clientes
+            var model = new List<Cliente>()
+            {
+                new Cliente(){ Id=1, Nome="Priscila", Idade=39},
+                new Cliente(){ Id=2, Nome="Raphael", Idade=18},
+                new Cliente(){ Id=3, Nome="Fabiano", Idade=38},
+                new Cliente(){ Id=4, Nome="Daniel", Idade=40},
+            };
+
+            return View(model);
+        }
+
+        public ActionResult JsonClientes()
+        {
+            //Acessar base de clientes
+            var model = new List<Cliente>()
+            {
+                new Cliente(){ Id=1, Nome="Priscila", Idade=39},
+                new Cliente(){ Id=2, Nome="Raphael", Idade=18},
+                new Cliente(){ Id=3, Nome="Fabiano", Idade=38},
+                new Cliente(){ Id=4, Nome="Daniel", Idade=40},
+            };
+
+            return Json(model, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Aula4/HelloWorld/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class TestController : Controller
+    {
+        public string Xpto()
+        {
+            return "Bateu no método XPTO";
+        }
+
+        public ViewResult Xyz()
+        {
+            //acesso a dados
+
+            //obter info webservice
+
+            return View();
+        }
+    }
+}
