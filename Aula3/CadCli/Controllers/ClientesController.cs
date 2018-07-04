67df4c497a2393284d8c03bd082d74a5d49f6379
Admin <Admin@GALACTUS>
2018-07-03 22:07:28 -0300

    adding jquery, ajax, javascript

--- Aula3/CadCli/Controllers/ClientesController.cs
+++ Aula3/CadCli/Controllers/ClientesController.cs
@@ -10,32 +10,29 @@ namespace CadCli.Controllers
 {
     public class ClientesController : Controller
     {
+        private static List<Cliente> _clientes = new List<Cliente>()
+        {
+            new Cliente(){ Id=1, Nome="Priscila", Idade=39},
+            new Cliente(){ Id=2, Nome="Raphael", Idade=18},
+            new Cliente(){ Id=3, Nome="Fabiano", Idade=38},
+            new Cliente(){ Id=4, Nome="Daniel", Idade=40},
+        };
+
         public ActionResult Index()
         {
-            //Acessar base de clientes
-            var model = new List<Cliente>()
-            {
-                new Cliente(){ Id=1, Nome="Priscila", Idade=39},
-                new Cliente(){ Id=2, Nome="Raphael", Idade=18},
-                new Cliente(){ Id=3, Nome="Fabiano", Idade=38},
-                new Cliente(){ Id=4, Nome="Daniel", Idade=40},
-            };
-
-            return View(model);
+            return View(_clientes);
         }
 
         public ActionResult JsonClientes()
         {
-            //Acessar base de clientes
-            var model = new List<Cliente>()
-            {
-                new Cliente(){ Id=1, Nome="Priscila", Idade=39},
-                new Cliente(){ Id=2, Nome="Raphael", Idade=18},
-                new Cliente(){ Id=3, Nome="Fabiano", Idade=38},
-                new Cliente(){ Id=4, Nome="Daniel", Idade=40},
-            };
+            return Json(_clientes, JsonRequestBehavior.AllowGet);
+        }
 
-            return Json(model, JsonRequestBehavior.AllowGet);
+        [HttpPost]
+        public ActionResult PostCliente(Cliente cliente)
+        {
+            _clientes.Add(cliente);
+            return Json(cliente, JsonRequestBehavior.DenyGet);
         }
     }
 }
