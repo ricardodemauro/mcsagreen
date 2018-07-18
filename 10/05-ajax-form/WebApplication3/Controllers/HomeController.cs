df9ae46485791bd87d6b95d53f3a59e31c532a24
Ricardo De Mauro <ricardo.demauro@coxautoinc.com>
2018-07-18 18:07:15 -0300

    adding ajax poc.

---
+++ 10/05-ajax-form/WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public PartialViewResult MyPartial()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public PartialViewResult MyPartialPost(Foto foto)
+        {
+            return PartialView(foto);
+        }
+
+        public PartialViewResult GetPartialResult()
+        {
+            return PartialView("FotoPartailView");
+        }
+    }
+}
