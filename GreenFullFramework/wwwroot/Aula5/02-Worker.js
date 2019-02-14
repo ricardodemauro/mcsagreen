2c6fe549e57d633d8c610232f0824b502f334a6a
Ricardo De Mauro <ricardo.demauro@coxautoinc.com>
2019-02-14 17:32:02 -0200

    session and local storage

---
+++ GreenFullFramework/wwwroot/Aula5/02-Worker.js
@@ -0,0 +1,17 @@
+﻿if (typeof (Worker) !== "undefined") {
+    // Yes! Web worker support!
+
+    let i = 0;
+
+    const timedCount = (counter) => {
+        counter += 1;
+
+        postMessage(counter);
+
+        setTimeout(timedCount, 500, counter);
+    };
+
+    i = timedCount(i);
+} else {
+    // Sorry! No Web Worker support..
+}
