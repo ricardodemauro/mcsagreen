cd5fa2d3c42d69a8bc9eb0dd66ed0830927e1405
ricardomauro <ricardo.demauro@gmail.com>
2019-02-14 22:00:51 -0200

    adicionando aula 4

---
+++ WebListaDeCompras/Controllers/ListaCompraController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebListaDeCompras.Models;
+
+namespace WebListaDeCompras.Controllers
+{
+    public class ListaCompraController : Controller
+    {
+        // GET: ListaCompra
+        public ActionResult Index()
+        {
+            ItemCompra abacate = new ItemCompra();
+            abacate.Nome = "Abacate";
+            abacate.Preco = 1.5m;
+
+            ItemCompra abacaxi = new ItemCompra();
+            abacaxi.Nome = "Abacaxi";
+            abacaxi.Preco = 2m;
+
+            List<ItemCompra> listaCompras = new List<ItemCompra>();
+            listaCompras.Add(abacate);
+            listaCompras.Add(abacaxi);
+
+            return View(listaCompras);
+        }
+    }
+}
