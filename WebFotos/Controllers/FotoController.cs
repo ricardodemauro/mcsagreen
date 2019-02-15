45264bf5a6e0f5f60cf99452b72f1304352d487b
ricardomauro <ricardo.demauro@gmail.com>
2019-02-15 20:28:53 -0200

    adding webfotos

---
+++ WebFotos/Controllers/FotoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebFotos.Controllers
+{
+    public class FotoController : Controller
+    {
+        // GET: Foto
+        public ActionResult Listar()
+        {
+            return View();
+        }
+
+        public ActionResult Upload()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebFotos/Models/Foto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebFotos.Models
+{
+    public class Foto
+    {
+        public int Id { get; set; }
+
+        public string Descricao { get; set; }
+
+        public string Titulo { get; set; }
+
+        public string Imagem { get; set; }
+
+        public DateTime DataPublicacao { get; set; }
+    }
+}
