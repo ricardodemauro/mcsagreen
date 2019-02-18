807304135fc3388d20ac38b0207b5f4593c6e24c
Ricardo De Mauro <ricardo.demauro@coxautoinc.com>
2019-02-18 17:57:59 -0300

    action filter e controller factory

---
+++ ExemploGreen.Web6/Infraestrutura/CustomerControllerFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.SessionState;
+
+namespace ExemploGreen.Web.Infraestrutura
+{
+    public class CustomerControllerFactory : IControllerFactory
+    {
+        private readonly IControllerFactory _default = new DefaultControllerFactory();
+
+        public IController CreateController(RequestContext requestContext, string controllerName)
+        {
+            return _default.CreateController(requestContext, controllerName);
+        }
+
+        public SessionStateBehavior GetControllerSessionBehavior(RequestContext requestContext, string controllerName)
+        {
+            return _default.GetControllerSessionBehavior(requestContext, controllerName);
+        }
+
+        public void ReleaseController(IController controller)
+        {
+            _default.ReleaseController(controller);
+        }
+    }
+}
---
+++ ExemploGreen.Web6/Infraestrutura/Filters/LogActionFilter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace ExemploGreen.Web.Infraestrutura.Filters
+{
+    public class LogActionFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            Debug.WriteLine($"Passing on {nameof(OnActionExecuting)}");
+            base.OnActionExecuting(filterContext);
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            Debug.WriteLine($"Passing on {nameof(OnActionExecuted)}");
+            base.OnActionExecuted(filterContext);
+        }
+    }
+}
