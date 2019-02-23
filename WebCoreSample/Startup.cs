d21fdb531a947acbab6e2147741f441f6590c7d9
ricardomauro <ricardo.demauro@gmail.com>
2019-02-22 22:21:59 -0300

    aula 10

--- WebCoreSample/Startup.cs
+++ WebCoreSample/Startup.cs
@@ -32,7 +32,10 @@ namespace WebCoreSample
             });
 
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc(cfg =>
+            {
+                cfg.Filters.Add(new RequireHttpsAttribute())
+            }).SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
