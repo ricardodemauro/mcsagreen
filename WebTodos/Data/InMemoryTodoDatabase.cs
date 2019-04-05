3bf5a4c8c464cdca1f83c9d91def6ffdea0f20d0
Ricardo De Mauro <ricardo.demauro@coxautoinc.com>
2019-04-05 17:51:18 -0300

    adding webtodos

---
+++ WebTodos/Data/InMemoryTodoDatabase.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebTodos.Models;
+
+namespace WebTodos.Data
+{
+    public class InMemoryTodoDatabase : ITodoDatabase
+    {
+        private readonly List<Todo> _db;
+
+        public InMemoryTodoDatabase()
+        {
+            _db = new List<Todo>();
+        }
+
+        public void AddTodo(Todo todo)
+        {
+            _db.Add(todo);
+        }
+
+        public List<Todo> GetAll()
+        {
+            return _db.ToList();
+        }
+
+        public Todo GetById(string id)
+        {
+            Guid otherId = Guid.Parse(id);
+            return _db.FirstOrDefault(c => c.Id.CompareTo(otherId) == 0);
+        }
+
+        public void UpdateTodo(string id, Todo todo)
+        {
+            Guid otherId = Guid.Parse(id);
+            var index = _db.FindIndex(x => x.Id.CompareTo(otherId) == 0);
+            if (index >= 0)
+            {
+                _db[index] = todo;
+            }
+        }
+    }
+}
---
+++ WebTodos/Models/Todo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebTodos.Models
+{
+    public class Todo
+    {
+        public Guid Id { get; set; }
+
+        public string Descricao { get; set; }
+
+        public bool Done { get; set; }
+    }
+}
