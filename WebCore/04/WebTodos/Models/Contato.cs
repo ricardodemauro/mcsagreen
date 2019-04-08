fee9da73cc912ccd609cedf48de35bf38637fdf0
Ricardo Mauro <ricardo.demauro@gmail.com>
2019-04-07 21:14:42 -0300

    adding validations

--- WebCore/04/WebTodos/Models/Contato.cs
+++ WebCore/04/WebTodos/Models/Contato.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WebTodos.Infra;
+using WebTodos.Infra.Validations;
 
 namespace WebTodos.Models
 {
@@ -11,9 +13,16 @@ namespace WebTodos.Models
     {
         public Guid Id { get; set; }
 
+        [Required(ErrorMessage = "Nome é obrigatório. Certo?")]
+        [AllLettersValidation(ErrorMessage = "Somente letras. Ok?")]
+        [Display(Name = "Nombre", ShortName = "Nom")]
         public string Nome { get; set; }
 
+        [DataType(DataType.PhoneNumber)]
         public string Telefone { get; set; }
+
+        [Range(0, 100)]
+        public int Age { get; set; }
 
         public GeoLocation Location { get; set; }
     }
